6277534422c152ecd474f926ac886800e9c15d00
vivekkrdixit <63907340+vivekkrdixit@users.noreply.github.com>
2020-04-20 19:13:21 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "instance_count" {
+  default = 1
+}
+
+variable "key_name" {
+  description = "Private key name to use with instance"
+  default     = "terraform"
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Base AMI to launch the instances"
+
+  default = "ami-01b01bbd08f24c7a8"
+}
