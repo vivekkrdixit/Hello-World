7c87910e1fb84e900e5b990767442dad91643d5f
vivekkrdixit <63907340+vivekkrdixit@users.noreply.github.com>
2020-04-22 21:45:47 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -4,7 +4,7 @@ variable "instance_count" {
 
 variable "key_name" {
   description = "Private key name to use with instance"
-  default     = "terraform"
+  default     = "Newest_Pair"
 }
 
 variable "instance_type" {
